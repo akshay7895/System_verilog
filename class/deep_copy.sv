dae6ae66ed8ae1355aabd443085a5816ce42d3dd
akshay purohit <45726518+akshay7895@users.noreply.github.com>
2023-06-15 02:28:09 +0530

    Update deep_copy.sv
    added some comments

--- class/deep_copy.sv
+++ class/deep_copy.sv
@@ -1,12 +1,13 @@
+//to implement deep copy we have to write copy method is all the class.
 module tb;
 
 class sub_class;
 	int a;
 	int b;
 	
-	function sub_class copy; //the function type is class name, function return the class object
-	copy = new;
-	copy.a = this.a;
+	function sub_class copy; //copy is handle, sub_class is the name of class 
+	copy = new; //object creation
+	copy.a = this.a; //copying the objects of class 
 	copy.b = this.b;
 	return copy;
 	endfunction
@@ -25,11 +26,11 @@ class ins_class;
 	$display("d=%d",d);
 	endfunction
 	
-	function ins_class copy; //function type is the class, in which it is called
+	function ins_class copy; //copy is handle, sub_class is the name of class
 	copy = new;
 	copy.c = this.c;
 	copy.d = this.d;
-	copy.s1 = s1.copy;
+	copy.s1 = s1.copy; //s1.copy it will call the copy method of class "sub_class"
 	return copy;
 	endfunction
 
@@ -46,7 +47,9 @@ h1.s1.a = 7;
 h1.s1.b = 8;
 $display("calling the h1 display method");
 h1.display();
-	h2 = h1.copy();  //will have to use this only .copy() method (can't use new here for deep copy)
+	h2 = h1.copy();  //have to use this syntax for deep copy -->new will work if the top class(here "ins_class") have only variable and no method(that will be shallow copy)
+	//.copy() so it will go to copy method of "ins_class" and allocate memory there and copy the objects mentioned there.
+	//c, d it will copy normally, then we have "copy.s1 = s1.copy" statement, so it will go to copy method of "sub_class" and create an object and copy the properties a,b and return 
 $display("calling the h2 display method");
 h2.display();
 h2.s1.a = 10;
