f0632394bfb4abf461a4422036c86ec2b0696aff
akshay purohit <45726518+akshay7895@users.noreply.github.com>
2023-06-16 18:11:43 +0530

    Create static_method.sv
    shows how static method works.

---
+++ class/static_method.sv
@@ -0,0 +1,30 @@
+module tb;
+	
+	
+	class tras;
+	static int ctr=0;
+	int a;
+	
+	function new();
+	ctr++;
+	$display("a=%d, ctr=%d",a,ctr); // "ctrl" will increment each time but not "a"
+	endfunction
+	
+	
+	static function dis ();
+	//$display("ctr=%d,a=%d",ctr,a); //compilation error as static method can't access non-static method
+	$display("ctr=%d",ctr);
+	endfunction
+	endclass
+	
+	tras t1,t2;
+	initial
+	begin
+	t1 = new;
+	tras::dis; //directly accessing the static method using class name 
+	t2 = new;
+	tras::dis;
+	
+	end
+	
+	endmodule
