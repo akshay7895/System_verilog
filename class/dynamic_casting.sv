19acda3ed7904e0a612047d1c256ed010aa99f93
akshay purohit <45726518+akshay7895@users.noreply.github.com>
2023-07-12 12:08:45 +0530

    Update dynamic_casting.sv
    changes comment.

--- class/dynamic_casting.sv
+++ class/dynamic_casting.sv
@@ -25,7 +25,7 @@ endclass
     begin
       b1 = new;
       a1 = b1; //child class assigned to parent class
-      $cast(b2,a1); //this is dynamic casting(in OOPs only static casting not possible)
+      $cast(b2,a1); //this is dynamic casting(in OOPs static casting not possible)
       //now we can assign the parent class to child class, bcz now the parent class is also same as child class
       b1.display();
       a1.display();
