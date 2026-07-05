4ff0b7d4218e3878b5d25fe880247e6a0daf5f72
akshay purohit <45726518+akshay7895@users.noreply.github.com>
2026-07-05 14:56:02 +0530

    methods explaination added

--- class/deep_copy.sv
+++ class/deep_copy.sv
@@ -1,4 +1,5 @@
 //to implement deep copy we have to write copy method is all the class.
+//Methods are not copied because they belong to the class definition and are shared by all objects.
 module tb;
 
 class sub_class;
@@ -47,7 +48,7 @@ h1.s1.a = 7;
 h1.s1.b = 8;
 $display("calling the h1 display method");
 h1.display();
-	h2 = h1.copy();  //have to use this syntax for deep copy -->new will work if the top class(here "ins_class") have only variable and no method(that will be shallow copy)
+	h2 = h1.copy();  //have to use this syntax for deep copy -->new will work if the top class(here "ins_class") have only variable and no class handles(that will be shallow copy)
 	//.copy() so it will go to copy method of "ins_class" and allocate memory there and copy the objects mentioned there.
 	//c, d it will copy normally, then we have "copy.s1 = s1.copy" statement, so it will go to copy method of "sub_class" and create an object and copy the properties a,b and return 
 $display("calling the h2 display method");
